5cbb69773f1acda485ef680bfaa48f8608e1b4a8
Dony Yuli Handoko <officialdonyyulihandoko@gmail.com>
2025-08-24 12:11:03 +0700

    commit final project

---
+++ resources/views/echo.blade.php
@@ -0,0 +1,5 @@
+<html>
+    <body>
+        <p>{{ $person }}</p>
+    </body>
+</html>
---
+++ tests/Feature/EchoTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Person;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class EchoTest extends TestCase
+{
+    public function testEcho()
+    {
+        $person = new Person;
+        $person->name = 'Dony';
+        $person->addres = 'Tangerang';
+
+        $this->view('echo', [
+            'person' => $person
+        ])->assertSeeText('Dony : Tangerang');
+    }
+}
